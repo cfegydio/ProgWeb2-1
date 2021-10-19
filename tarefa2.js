cd56116072ae3cd9f748db047b7cfed284c87f1b
eduolmo <91472785+eduolmo@users.noreply.github.com>
2021-10-19 08:59:20 -0300

    Update tarefa2.js

--- tarefa2.js
+++ tarefa2.js
@@ -2,24 +2,11 @@ const subtitulos = document.querySelectorAll("h2");
 const topicos = new Array();
 subtitulos.forEach(element => {
 	topicos.push(element.textContent);
-	
-	const ancora = createElement("a");
-	ancora.setAttribute("name", element.textContent);
-	element.append(ancora);
-	
-	const retorno = document.createElement("a");
-	retorno.setAttribute("href", "#");
-	retorno.textContent = "início";
-	element.parentElement.insertBefore(retorno, element.nextElementSibling);
 });
 
 const lista = document.querySelector("ol");
 topicos.forEach(topico => {
   const item = document.createElement("li");
-  lista.append(item);
-  
-  const link = document.createElement("a");
-  link.setAttribute("href",`#${topico}`)
   item.textContent = topico;
-  item.append(link);
+  lista.append(item);
 });
