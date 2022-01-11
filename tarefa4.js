70e4ad13d65526a7a5ae8b85f7c9c16644ad5202
eduolmo <91472785+eduolmo@users.noreply.github.com>
2022-01-11 16:42:37 -0300

    Add files via upload

---
+++ tarefa4.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", criaBotao)
+let tamanho = 2;
+
+function criaBotao(){
+    div = document.createElement("div");
+    div.style.fontSize = `${tamanho}em`;
+    div.innerHTML = `<button onclick="aumentaTitulo()"> + </button>
+    <button onclick="diminuiTitulo()"> - </button>`;
+    document.body.prepend(div)
+}
+
+function atualizaTamanho() {
+    const h1 = document.querySelector("h1");
+    h1.style.fontSize = `${tamanho}em`;
+}
+
+function aumentaTitulo() {
+    if (tamanho < 5) {
+        tamanho += 0.5;
+        atualizaTamanho();
+    }
+}
+
+function diminuiTitulo() {
+    if (tamanho > 1) {
+        tamanho -= 0.5;
+        atualizaTamanho();
+    }
+}
+
