a341bb8008ec5f0ae9b721e1924480a0508a93a2
eduolmo <91472785+eduolmo@users.noreply.github.com>
2022-01-17 15:21:33 -0300

    Update damas.js

--- damas.js
+++ damas.js
@@ -1,6 +1,6 @@
 const tamanhoCelula = 40;
 document.body.append(criaTabuleiro());
-document.body.background = 'img/aquarela.jpg';
+document.body.background = 'img\aquarela.jpg';
 
 function criaTabuleiro() {
     const tamanho = 8;
