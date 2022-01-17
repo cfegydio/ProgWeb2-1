da63360ec5db17a41713afa155535d71ccd0dcca
eduolmo <91472785+eduolmo@users.noreply.github.com>
2022-01-17 14:33:42 -0300

    Add files via upload

---
+++ damas.js
@@ -0,0 +1,88 @@
+const tamanhoCelula = 40;
+let pecaId = 0;
+document.body.append(criaTabuleiro());
+
+
+function criaTabuleiro() {
+    const tamanho = 8;
+    let tabela = document.createElement('table');
+
+    tabela.style.borderStyle = 'solid';
+    tabela.style.borderSpacing = 0;
+    tabela.style.margin = 'auto';
+    tabela.style.borderColor = 'orange';
+
+    for (let i = 0; i < tamanho; i++) {
+        let linha = document.createElement('tr');
+        tabela.append(linha);
+        for (let j = 0; j < tamanho; j++) {
+            let celula = document.createElement('td');
+            linha.append(celula);
+            celula.style.width = `${tamanhoCelula}px`;
+            celula.style.height = `${tamanhoCelula}px`;
+            if (i % 2 == j % 2) {
+                celula.style.backgroundColor = '#00BECC';
+                celula.classList.add("container");
+                if (i * 8 + j <= 24) {
+                    celula.append(criaPeca('black'));
+                } else if (i * 8 + j >= 40) {
+                    celula.append(criaPeca('red'));
+                }
+            } else {
+                celula.style.backgroundColor = 'white';
+            }
+        }
+    };
+    return tabela;
+}
+
+function criaPeca(cor) {
+    let imagem = document.createElement('img');
+    imagem.setAttribute('src', `img/${cor}.png`);
+    imagem.setAttribute('width', `${tamanhoCelula-4}px`);
+    imagem.setAttribute('height', `${tamanhoCelula-4}px`);
+    imagem.setAttribute('draggable','true');
+    imagem.classList.add("draggable");
+    return imagem;
+}
+
+const draggables = document.querySelectorAll(".draggable");
+const containers = document.querySelectorAll(".container");
+
+draggables.forEach(draggable => {
+    draggable.addEventListener("dragstart", () => {
+        draggable.classList.add("dragging");
+        
+    });
+    draggable.addEventListener("dragend", () => {
+        draggable.classList.remove("dragging");
+    });
+});
+
+containers.forEach(container => {
+    container.addEventListener("dragover", e => {
+        e.preventDefault();
+    });
+    container.addEventListener("drop", () => {
+        const draggable = document.querySelector(".dragging");
+        container.appendChild(draggable);
+    });
+});
+
+
+
+/*
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+  */
